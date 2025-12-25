08b878d8bc18b90c3bf78da3df0a47b5f913363f
tekofx <tekofxx@gmail.com>
2025-12-25 14:25:58 +0100

    Refactored code

--- main.c
+++ main.c
@@ -5,53 +5,49 @@
 #define SCREEN_WIDTH 480
 #define SCREEN_HEIGHT 272
 
+void renderImage(SDL_Renderer *renderer, SDL_Texture *sprite) {
+  SDL_Rect sprite_rect;
+  SDL_QueryTexture(sprite, NULL, NULL, &sprite_rect.w, &sprite_rect.h);
+  sprite_rect.w = 150;
+  sprite_rect.h = 150;
+  sprite_rect.x = 0;
+  sprite_rect.y = (SCREEN_HEIGHT - sprite_rect.h) / 2;
+  SDL_RenderCopyEx(renderer, sprite, NULL, &sprite_rect, 270, NULL,
+                   SDL_FLIP_NONE);
+}
+
+void renderText(SDL_Renderer *renderer, SDL_Texture *textTexture) {
+  SDL_Rect textRect;
+  SDL_QueryTexture(textTexture, NULL, NULL, &textRect.w, &textRect.h);
+  textRect.x = (SCREEN_WIDTH - textRect.w) / 2;
+  textRect.y = (SCREEN_HEIGHT - textRect.h) / 2;
+  SDL_RenderCopyEx(renderer, textTexture, NULL, &textRect, 270, NULL,
+                   SDL_FLIP_NONE);
+}
+
 int main(int argc, char *argv[]) {
   SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER);
-
-  // Enable png support for SDL2_image
   IMG_Init(IMG_INIT_PNG);
-
-  // Enable ttf support
   TTF_Init();
 
   SDL_Window *window =
       SDL_CreateWindow("window", SDL_WINDOWPOS_UNDEFINED,
                        SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-
   SDL_Renderer *renderer =
       SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-  // Load the texture
-  SDL_Surface *pixels = IMG_Load("image.png");
-  SDL_Texture *sprite = SDL_CreateTextureFromSurface(renderer, pixels);
-  SDL_FreeSurface(pixels);
+  // Load image
+  SDL_Surface *imgSurface = IMG_Load("image.png");
+  SDL_Texture *sprite = SDL_CreateTextureFromSurface(renderer, imgSurface);
+  SDL_FreeSurface(imgSurface);
 
-  // Load the ttf
-  TTF_Font *font = TTF_OpenFont("font.ttf", 24); // Load font
-  SDL_Color textColor = {0, 0, 0, 255};
-  SDL_Surface *textSurface =
-      TTF_RenderText_Solid(font, "Center Text", textColor);
+  // Load font and render text
+  TTF_Font *font = TTF_OpenFont("font.ttf", 24);
+  SDL_Color color = {0, 0, 0, 255};
+  SDL_Surface *textSurface = TTF_RenderText_Solid(font, "Center Text", color);
   SDL_Texture *textTexture =
       SDL_CreateTextureFromSurface(renderer, textSurface);
   SDL_FreeSurface(textSurface);
-
-  // Store the dimensions of the sprite
-  SDL_Rect sprite_rect;
-  SDL_QueryTexture(sprite, NULL, NULL, &sprite_rect.w, &sprite_rect.h);
-
-  // Define size and position of sprite
-  sprite_rect.w = 150; // new width
-  sprite_rect.h = 150; // new height
-  sprite_rect.x = 0;
-  sprite_rect.y = (SCREEN_HEIGHT - sprite_rect.h) / 2;
-
-  // Store the dimensions of the text
-  SDL_Rect textRect;
-  SDL_QueryTexture(textTexture, NULL, NULL, &textRect.w, &textRect.h);
-
-  // Define position of the text
-  textRect.x = (480 - textRect.w) / 2;
-  textRect.y = (272 - textRect.h) / 2;
 
   int running = 1;
   SDL_Event event;
@@ -76,23 +72,21 @@ int main(int argc, char *argv[]) {
       }
     }
 
-    // Clear the screen
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
 
-    // Draw sprite
-    SDL_RenderCopyEx(renderer, sprite, NULL, &sprite_rect, 270, NULL,
-                     SDL_FLIP_NONE);
+    renderImage(renderer, sprite);
+    renderText(renderer, textTexture);
 
-    // Draw the text
-    SDL_RenderCopyEx(renderer, textTexture, NULL, &textRect, 270, NULL,
-                     SDL_FLIP_NONE);
-
-    // Draw everything on a white background
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderPresent(renderer);
   }
+  SDL_DestroyTexture(textTexture);
+  SDL_DestroyTexture(sprite);
+  TTF_CloseFont(font);
+  TTF_Quit();
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
+  IMG_Quit();
   SDL_Quit();
 
   return 0;
